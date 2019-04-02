29192e0c5ec5830104a651733a54307de39aab90
Gavin O'Connor <gedmund@gmail.com>
2019-04-02 23:34:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, someKey, someValue) {
 
 function deleteFromObjectByKey(recipes, someKey) {
   let newObj = Object.assign({}, recipes, someKey);
-  delete newObj.someKey;
+  delete newObj[someKey];
   return newObj;
 }
 
