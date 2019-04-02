448a3ac9b8b551453d24bbcba3c741af2057a772
Gavin O'Connor <gedmund@gmail.com>
2019-04-02 23:11:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(recipes, someKey, someValue) {
-  Object.assign(recipes, {[someKey]: someValue});
+  Object.assign({}, recipes, {[someKey]: someValue});
   return recipes;
 }
 
