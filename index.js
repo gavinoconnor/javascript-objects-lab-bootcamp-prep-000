4693303c99f7ee0ab1c005b6a9a1bb76c96fe717
Gavin O'Connor <gedmund@gmail.com>
2019-04-02 23:09:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,25 +3,17 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(recipes, someKey, someValue) {
-  return Object.assign({}, recipes, {[someKey]: someValue})
+  Object.assign(recipes, {[someKey]: someValue});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, someKey, someValue) {
-  recipes[someKey] = someValue
-  return recipes
+
 }
 
 function deleteFromObjectByKey(recipes, someKey) {
-  var newRecipes = Object.assign({}, recipes)
-  delete newRecipes[someKey]
-  return newRecipes
+
 }
 
 function destructivelyDeleteFromObjectByKey(recipes, someKey) {
-  delete recipes[someKey]
-  return recipes
-}
 
-// function destructivelyDeleteFromObjectByKey () {
-//
-// }
+}
